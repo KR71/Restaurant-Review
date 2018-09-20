f87f741624cdbc03cd9023c64f23fda2bee2d98d
Kevin Rollins <kevin.rollins71@googlemail.com>
2018-09-20 17:55:56 +0100

    Feature: added cache and worker for page get requests

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,23 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('restaurant-review').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/js/main.js',
+                '/js/restaurant_info.js'
+            ]);
+        })
+    );
+});
+
 self.addEventListener('fetch', function(event) {
-    console.log(event.request);
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request.url).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
