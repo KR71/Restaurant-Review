a539131e8fbac3121e2a0c82e42bff009944fdd6
Kevin Rollins <kevin.rollins71@googlemail.com>
2018-09-25 23:08:33 +0100

    Feature: More accessibility added, amended readme

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,21 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/serviceWorker.js')
+        .then(function() { console.log("Service Worker Registered"); });
+}
+
+
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('restaurant-review').then(function(cache) {
             return cache.addAll([
                 '/',
                 '/index.html',
+                '/restaurant.html',
                 '/css/styles.css',
                 '/js/main.js',
                 '/js/restaurant_info.js'
+
             ]);
         })
     );
