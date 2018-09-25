fc45fa3cf07f65c71a577a04f79f8d2058953fff
Kevin Rollins <kevin.rollins71@googlemail.com>
2018-09-25 21:51:14 +0100

    Feature: added responsiveness on review page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -87,11 +87,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-
   image.className = 'restaurant-img';
+  image.setAttribute('alt', 'Picture of Restaurant');
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
-
 
 
   const cuisine = document.getElementById('restaurant-cuisine');
